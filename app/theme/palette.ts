133d974dc05bb151199dcbf06ab22bd29530f4b0
duchm <duchm@duchms-Mac-mini.local>
2021-06-24 14:16:05 +0700

    init

---
+++ app/theme/palette.ts
@@ -0,0 +1,20 @@
+export const palette = {
+  white: "#ffffff",
+  offWhite: "#e6e6e6",
+  orange: "#FBA928",
+  orangeDarker: "#EB9918",
+  lightGrey: "#939AA4",
+  lighterGrey: "#CDD4DA",
+  angry: "#dd3333",
+  deepPurple: "#5D2555",
+
+  // CyStack
+  green: "#268334",
+  black: "#072245",
+  blackTitle: "#031B4E",
+  gray: "#8499B2",
+  grayText: "#617296",
+  danger: "#F54F64",
+  warning: "#FF7E24",
+  success: "#3DB249"
+}
