07880d48ea071324c3a5819a1f7fb349f1b1bb8a
duchm <duchm@duchms-Mac-mini.local>
2021-06-29 09:02:49 +0700

    WIP

--- app/screens/login-1/login-1-screen.tsx
+++ app/screens/login-1/login-1-screen.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { SafeAreaView, View, ViewStyle } from "react-native"
-import { Button, Screen, Text } from "../../components"
+import { AutoImage as Image, Button, Text } from "../../components"
 import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color, spacing } from "../../theme"
 
 const SECTION_PADDING: ViewStyle = {
   paddingVertical: spacing[2],
-  paddingHorizontal: spacing[4]
+  paddingHorizontal: spacing[4],
 }
 
 export const Login1Screen = observer(function Login1Screen() {
@@ -22,15 +22,37 @@ export const Login1Screen = observer(function Login1Screen() {
     <View style={{ flex: 1 }}>
       <SafeAreaView />
 
-      <View style={[SECTION_PADDING, { flex: 1 }]}>
-        <Text text="login" />
+      <View style={[SECTION_PADDING, { flex: 1, justifyContent: "center", alignItems: "center" }]}>
+        <Image source={require("./logo.png")} />
+        <Text
+          text="Your daily needs"
+          style={{ fontSize: 14, marginTop: 31, color: color.palette.blackTitle }}
+        />
       </View>
 
       <SafeAreaView>
         <View style={SECTION_PADDING}>
+          <Button text="Login" onPress={() => navigation.navigate("intro")} />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginTop: 12,
+          }}
+        >
+          <Text
+            text="Don't have an account?"
+            style={{
+              fontSize: 14,
+              marginRight: 8,
+            }}
+          />
           <Button
-            text="Login"
-            onPress={() => navigation.navigate("intro")}
+            preset="link"
+            text="Sign up"
+            textStyle={{ color: color.palette.green, fontSize: 14 }}
+            onPress={() => navigation.navigate("login-1")}
           />
         </View>
       </SafeAreaView>
