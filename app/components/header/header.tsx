c822fc16393b22fdbab4ddf17d45bc9e51ec7fa9
duchunter <duc71098@gmail.com>
2021-08-07 13:58:20 +0700

    update navigation tabs

---
+++ app/components/header/header.tsx
@@ -0,0 +1,65 @@
+import * as React from "react"
+import { StyleProp, View, ViewStyle } from "react-native"
+import { observer } from "mobx-react-lite"
+import { Text, Button, AutoImage as Image } from "../"
+import { flatten } from "ramda"
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { color } from "../../theme"
+
+const CONTAINER: ViewStyle = {
+  flexDirection: 'row',
+  justifyContent: "space-between",
+  alignItems: 'center'
+}
+
+export interface HeaderProps {
+  children?: React.ReactNode,
+  style?: StyleProp<ViewStyle>,
+  title?: string,
+  left?: JSX.Element,
+  right?: JSX.Element,
+  goBack?: Function,
+  showLogo?: boolean
+}
+
+/**
+ * Describe your component here
+ */
+export const Header = observer(function Header(props: HeaderProps) {
+  const { style } = props
+  const styles = flatten([CONTAINER, style])
+
+  return (
+    <View> 
+      <View style={styles}>
+        {
+          props.left 
+            ? props.left 
+            : props.goBack ? (
+              <Button preset="link" onPress={() => props.goBack()}>
+                  <Icon 
+                    name="arrow-left"
+                    size={16} 
+                    color={color.title} 
+                  />
+              </Button>
+            ) : props.showLogo && (
+              <Image source={require('./locker-logo.png')} style={{ height: 24 }} />
+            )
+        }
+        {
+          props.title && (
+          <Text preset="semibold">
+            {props.title}
+          </Text>
+          )
+        }
+        {
+          props.right && props.right
+        }
+      </View>
+
+      {props.children}
+    </View>
+  )
+})
