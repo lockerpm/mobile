be8fbb05370e44232453881922519d7697566f54
Duc Ha <duchm@Ducs-Mac.local>
2021-09-21 10:29:59 -0700

    update production ios

--- ios/CyStackLocker/AppDelegate.m
+++ ios/CyStackLocker/AppDelegate.m
@@ -50,7 +50,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"HelloWorld"
+                                                   moduleName:@"CyStackLocker"
                                             initialProperties:nil];
 
     if (@available(iOS 13.0, *)) {
