68b4b7d9f7b314370b4cbe5db1ce9bbdde608d14
duchm <duchm@duchms-Mac-mini.local>
2021-10-30 17:57:51 +0700

    WIP: tool 2

--- app/screens/auth/tools/password-health/password-health-screen.tsx
+++ app/screens/auth/tools/password-health/password-health-screen.tsx
@@ -1,25 +1,90 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../../../components"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "../../models"
-import { color } from "../../../../theme"
+import { View } from "react-native"
+import { useNavigation } from "@react-navigation/core"
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
+import { useMixins } from "../../../../services/mixins"
+import { Button, Header, Layout, Text } from "../../../../components"
+import { color, commonStyles, fontSize } from "../../../../theme"
 
-const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
-  flex: 1,
-}
+// @ts-ignore
+import DataBreachScannerIcon from './data-breach-scanner.svg'
+
 
 export const PasswordHealthScreen = observer(function PasswordHealthScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const { translate } = useMixins()
+  const navigation = useNavigation()
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  // -------------------- RENDER ----------------------
+
+  const renderOption = (title: string, desc: string, left: React.ReactNode, bordered?: boolean) => (
+    <Button
+      preset="link"
+      onPress={() => {
+        
+      }}
+      style={[commonStyles.CENTER_HORIZONTAL_VIEW, {
+        borderBottomColor: color.line,
+        borderBottomWidth: bordered ? 1 : 0,
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+      }]}
+    >
+      <View style={[commonStyles.CENTER_HORIZONTAL_VIEW, {
+        flex: 1
+      }]}>
+        { left }
+
+        <View style={{ paddingHorizontal: 10, flex: 1 }}>
+          <Text
+            preset="black"
+            text={title}
+          />
+          <Text
+            text={desc}
+            style={{
+              marginTop: 3,
+              fontSize: fontSize.small
+            }}
+          />
+        </View>
+      </View>
+      
+      <FontAwesomeIcon
+        name="angle-right"
+        size={18}
+        color={color.textBlack}
+      />
+    </Button>
+  )
+
   return (
-    <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="" />
-    </Screen>
+    <Layout
+      containerStyle={{
+        backgroundColor: color.block,
+        paddingHorizontal: 0
+      }}
+      header={(
+        <Header
+          title={translate('pass_health.title')}
+          goBack={() => navigation.goBack()}
+          right={(
+            <View style={{ width: 10 }} />
+          )}
+        />
+      )}
+    >
+      <View style={commonStyles.GRAY_SCREEN_SECTION}>
+        {
+          renderOption(
+            'Exposed Passwords',
+            'Passwords that have been uncovered in known data breaches',
+            (
+              <DataBreachScannerIcon height={40} width={40} />
+            )
+          )
+        }
+      </View>
+    </Layout>
   )
 })
