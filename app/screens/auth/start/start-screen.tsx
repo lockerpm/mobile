b9bcb00ca28f2c624c6741076ab9f5c31282515a
duchm <duchm@duchms-Mac-mini.local>
2021-11-03 15:06:19 +0700

    update start screen loading message

--- app/screens/auth/start/start-screen.tsx
+++ app/screens/auth/start/start-screen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { Loading } from "../../../components"
 import { useNavigation } from "@react-navigation/native"
@@ -15,8 +15,11 @@ export const StartScreen = observer(function StartScreen() {
   } = useMixins()
   const navigation = useNavigation()
 
+  const [msg, setMsg] = useState('')
+
   const mounted = async () => {
     if (!uiStore.isOffline) {
+      setMsg(translate('start.synching'))
       await delay(500)
       const [syncRes, invitationsRes] = await Promise.all([
         getSyncData(),
@@ -38,10 +41,10 @@ export const StartScreen = observer(function StartScreen() {
       if (invitationsRes.kind == 'ok') {
         user.setInvitations(invitationsRes.data)
       }
-
-      await delay(500)
     }
     
+    setMsg(translate('start.decrypting'))
+    await delay(500)
     await Promise.all([
       loadFolders(),
       loadCollections()
@@ -54,6 +57,7 @@ export const StartScreen = observer(function StartScreen() {
       navigation.navigate('switchDevice')
     }
 
+    setMsg('')
     const introShown = await load(storageKeys.APP_SHOW_BIOMETRIC_INTRO)
     if (!introShown) {
       const available = await isBiometricAvailable()
@@ -72,6 +76,6 @@ export const StartScreen = observer(function StartScreen() {
   }, [])
 
   return (
-    <Loading />
+    <Loading message={msg} />
   )
 })
