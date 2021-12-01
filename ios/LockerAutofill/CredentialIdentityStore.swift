bc076ea1c42169ff104867c1df7c0dea7e46d51a
Nguyen Thinh <thinhnn@Nguyens-Mac-mini.local>
2021-12-01 15:09:00 +0700

    add search field

--- ios/LockerAutofill/CredentialIdentityStore.swift
+++ ios/LockerAutofill/CredentialIdentityStore.swift
@@ -8,16 +8,50 @@
 import Foundation
 import KeychainAccess
 
-class CredentialIdentityStore {
-  let KEYCHAIN_SERVICE: String = "W7S57TNBH5.com.cystack.lockerapp"
-  let KEYCHAIN_ACCESS_GROUP: String = "group.com.cystack.lockerapp"
-  
-  let URI: String
-  var passwords: [[String: String]] = []
-  
-  init(uri: String) {
-    self.URI = uri
+struct PasswordCredential {
+  var username: String
+  var password: String
+  init(_ username: String, _ password: String) {
+    self.username = username
+    self.password = password
   }
+}
+
+class CredentialIdentityStore {
+  private let KEYCHAIN_SERVICE: String = "W7S57TNBH5.com.cystack.lockerapp"
+  private let KEYCHAIN_ACCESS_GROUP: String = "group.com.cystack.lockerapp"
+  
+  private let KEYCHAIN_PROPS: String = "autofill"
+  private var autofillData: String
+  private var keychain: Keychain
+  
+  
+  var credentials: [[String: String]] = []
+  let URI: String
+  
+  
+  init(_ uri: String) {
+    self.URI = URL(string: uri)?.host ?? ""
+    
+    var passwords: [[String: String]] = []
+    // Get data from shared keychain
+    self.keychain = Keychain(service: KEYCHAIN_SERVICE, accessGroup: KEYCHAIN_ACCESS_GROUP)
+    self.autofillData = try! keychain.get(KEYCHAIN_PROPS) ?? "[]"
+    print("Autofill data", self.autofillData)
+    passwords = self.toArray(text: self.autofillData) ?? []
+    
+    // Buttons
+    for (_, item) in passwords.enumerated() {
+      let cipherUri = item["uri"] ?? ""
+      
+      if uri.isEmpty || uri.contains(cipherUri.lowercased()) {
+        self.credentials.append(item)
+      }
+    }
+    print(self.credentials)
+  }
+  
+  
   
   
   private func toArray(text: String) -> [[String: String]]? {
--- ios/LockerAutofill/CredentialProviderViewController.swift
+++ ios/LockerAutofill/CredentialProviderViewController.swift
@@ -6,27 +6,23 @@
 //
 
 import AuthenticationServices
-import KeychainAccess
 
 
-func toArray(text: String) -> [[String: String]]? {
-  if let data = text.data(using: .utf8) {
-    do {
-      return try JSONSerialization.jsonObject(with: data, options: []) as? [[String:String]]
-    } catch {
-      print(error.localizedDescription)
-    }
-  }
-  return []
-}
 
-
-class CredentialProviderViewController: ASCredentialProviderViewController {
+class CredentialProviderViewController: ASCredentialProviderViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate{
   
-  @IBOutlet weak var stackView: UIStackView!
-  @IBOutlet weak var loading: UIActivityIndicatorView!
-  var passwords: [[String: String]] = []
+  
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var uriNotification: UILabel!
+  
+  
+    var credentialIdStore: CredentialIdentityStore!
+    var filterData:  [[String: String]] = []
+   // var data:  [[String: String]] = []
     
+  
+  
   func makeButton(title: String, index: Int) -> UIButton {
     var configuration = UIButton.Configuration.plain()
     configuration.buttonSize = .large
@@ -45,116 +41,114 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
     label.text = text
     label.font = label.font?.withSize(CGFloat(size))
     label.textColor = color
+    label.numberOfLines = 0
+    label.lineBreakMode = .byWordWrapping
     return label
   }
 
   @objc func pressed(sender: UIButton) {
-    let target = self.passwords[sender.tag]
+    let target = self.credentialIdStore.credentials[sender.tag]
     let passwordCredential = ASPasswordCredential(user: target["username"] ?? "", password: target["password"] ?? "")
     self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
   }
+ 
 
-  /*
-   Prepare your UI to list available credentials for the user to choose from. The items in
-   'serviceIdentifiers' describe the service the user is logging in to, so your extension can
-   prioritize the most relevant credentials in the list.
-  */
   override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
-    print("Debug")
+ // for search
+    self.searchBar.delegate = self
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+    
+      
     // Get uri
     var uri = ""
-    print(serviceIdentifiers.count)
-    print(serviceIdentifiers[0])
     if serviceIdentifiers.count > 0 {
       uri = serviceIdentifiers[0].identifier
     }
-    print(uri)
     
-    // Clear view
-    self.stackView.subviews.forEach({ $0.removeFromSuperview() })
+    // get audofill data from shared keychain suitable with uri
+    self.credentialIdStore = CredentialIdentityStore(uri)
+    uriNotification.text = "Choose a password to use for \"\(self.credentialIdStore.URI)\""
+    uriNotification.font = uriNotification.font?.withSize(CGFloat(14))
+    //clear stackView
     
-    // Get data from shared keychain
-    let keychain = Keychain(service: "W7S57TNBH5.com.cystack.lockerapp", accessGroup: "group.com.cystack.lockerapp")
-    let autofillData = try? keychain.get("autofill")
-    print(autofillData!)
+    self.filterData = self.credentialIdStore.credentials
+    tableView.reloadData()
     
-    // Has passwords
-    if autofillData != nil {
-      self.passwords = toArray(text: autofillData ?? "[]") ?? []
-      print(self.passwords)
-      if (self.passwords.count > 0) {
-        var hasItem = false
-        
-        // Label
-        let label = makeLabel(text: "Credentials for \(uri)\n", size: 20)
-        label.textAlignment = .center
-        self.stackView.addArrangedSubview(label)
-        self.stackView.setCustomSpacing(CGFloat(15), after: label)
-        
-        // Buttons
-        for (index, item) in self.passwords.enumerated() {
-          let cipherUri = item["uri"] ?? ""
-          print(cipherUri)
-          if uri.isEmpty || uri.contains(cipherUri) {
-            hasItem = true
-            let btn = self.makeButton(title: item["username"] ?? "", index: index)
-            self.stackView.addArrangedSubview(btn)
-          }
-        }
-        
-        if hasItem {
-          return
-        }
+//
+//    if credentialIdStore.credentials.count > 0 {
+//      //   label
+//      let label = makeLabel(text: "Credentials for \(credentialIdStore.URI)\n", size: 20)
+//      label.textAlignment = .left
+//      self.stackView.addArrangedSubview(label)
+//      self.stackView.setCustomSpacing(CGFloat(5), after: label)
+//
+//      // add credential items
+//      for (index, item) in self.credentialIdStore.credentials.enumerated() {
+//          let btn = self.makeButton(title: item["username"] ?? "", index: index)
+//          self.stackView.addArrangedSubview(btn)
+//      }
+//      return
+//    }
+//
+//    // No password available
+//    let label = makeLabel(text: "There are no passwords available for this uri. Add a password to Locker to start using autofill feature.", size: 16)
+//    label.frame = CGRect(x: 16, y: 0, width: UIScreen.main.bounds.size.width-60, height: 100.0)
+//    label.textAlignment = .center
+//    self.stackView.addSubview(label)
+    //stackView.alignment = .center
+  }
+
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.filterData.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+    
+    cell.textLabel?.text = self.filterData[indexPath.row]["username"]
+
+    return cell
+  }
+  func numberOfSections(in tableView: UITableView) -> Int {
+    
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+  }
+  
+  //mark search bar config
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    filterData = []
+    if searchText == "" {
+      filterData = credentialIdStore.credentials
+    }
+    for credential in credentialIdStore.credentials {
+      let username: String = credential["username"]!.lowercased()
+      if username.contains(searchText.lowercased()) {
+          filterData.append(credential)
       }
     }
+     self.tableView.reloadData()
+  }
     
-    // No password available
-    let label = makeLabel(text: "There are no passwords available for this uri. Add a password to Locker to start using autofill feature.", size: 16)
-    label.numberOfLines = 0
-    self.stackView.addArrangedSubview(label)
-  }
-
-//
-//   Implement this method if your extension supports showing credentials in the QuickType bar.
-//   When the user selects a credential from your app, this method will be called with the
-//   ASPasswordCredentialIdentity your app has previously saved to the ASCredentialIdentityStore.
-//   Provide the password by completing the extension request with the associated ASPasswordCredential.
-//   If using the credential would require showing custom UI for authenticating the user, cancel
-//   the request with error code ASExtensionError.userInteractionRequired.
-//
-//  override func provideCredentialWithoutUserInteraction(for credentialIdentity: ASPasswordCredentialIdentity) {
-//    
-//     
-//      let passwordCredential = ASPasswordCredential(user: "j_appleseed", password: "apple1234")
-//      self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
-//      
-//  }
-//  
-
-  /*
-   Implement this method if provideCredentialWithoutUserInteraction(for:) can fail with
-   ASExtensionError.userInteractionRequired. In this case, the system may present your extension's
-   UI and call this method. Show appropriate UI for authenticating the user then provide the password
-   by completing the extension request with the associated ASPasswordCredential.
-
-  override func prepareInterfaceToProvideCredential(for credentialIdentity: ASPasswordCredentialIdentity) {
-  }
-  */
-
-  @IBAction func cancel(_ sender: AnyObject?) {
+    @IBAction func cancel(_ sender: AnyObject?) {
     self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.userCanceled.rawValue))
   }
   
   // move to Add pass screen
   @IBAction func add() {
-    print("add")
+
     let addPassView = storyboard?.instantiateViewController(withIdentifier: "AddNewPasswordView") as! NewPasswordViewController
-    addPassView.completionHandler = {text1, text2 in
-      print(text1, text2)
-    }
+//    addPassView.completionHandler = {text1, text2 in
+//      print(text1, text2)
+//    }
     present(addPassView, animated: true)
   }
-//  @IBAction func addNewPassword(_ sender: Any) {
-//    add()
-//  }
+  
+  @IBAction func addNewPassword(_ sender: Any) {
+    add()
+  }
 }
+
+
