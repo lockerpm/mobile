5770b11856e1c195561360202383564ead5b43cf
Nguyen Thinh <thinhnn@Nguyens-Mac-mini.local>
2021-12-02 20:00:00 +0700

    update main UI, add new pass UI

--- ios/LockerAutofill/CredentialProviderViewController.swift
+++ ios/LockerAutofill/CredentialProviderViewController.swift
@@ -9,56 +9,23 @@ import AuthenticationServices
 
 
 
-class CredentialProviderViewController: ASCredentialProviderViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate{
-  
+class CredentialProviderViewController: ASCredentialProviderViewController {
   
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var uriNotification: UILabel!
   
-  
     var credentialIdStore: CredentialIdentityStore!
-    var filterData:  [[String: String]] = []
+    var filterCredentials:  [[String: String]] = []
+    var filterOthers: [[String: String]] = []
    // var data:  [[String: String]] = []
     
-  
-  
-  func makeButton(title: String, index: Int) -> UIButton {
-    var configuration = UIButton.Configuration.plain()
-    configuration.buttonSize = .large
-    
-    let button = UIButton(configuration: configuration)
-    button.tag = index
-    button.setTitle(title, for: .normal)
-    button.setTitleColor(.link, for: .normal)
-    button.contentHorizontalAlignment = .left
-    button.addTarget(self, action: #selector(pressed), for: .touchUpInside)
-    return button
-  }
-  
-  func makeLabel(text: String, size: Int, color: UIColor? = .label) -> UILabel {
-    let label = UILabel()
-    label.text = text
-    label.font = label.font?.withSize(CGFloat(size))
-    label.textColor = color
-    label.numberOfLines = 0
-    label.lineBreakMode = .byWordWrapping
-    return label
-  }
-
-  @objc func pressed(sender: UIButton) {
-    let target = self.credentialIdStore.credentials[sender.tag]
-    let passwordCredential = ASPasswordCredential(user: target["username"] ?? "", password: target["password"] ?? "")
-    self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
-  }
- 
 
   override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
- // for search
+    // for search
     self.searchBar.delegate = self
     self.tableView.delegate = self
     self.tableView.dataSource = self
-    
       
     // Get uri
     var uri = ""
@@ -68,77 +35,22 @@ class CredentialProviderViewController: ASCredentialProviderViewController, UISe
     
     // get audofill data from shared keychain suitable with uri
     self.credentialIdStore = CredentialIdentityStore(uri)
-    uriNotification.text = "Choose a password to use for \"\(self.credentialIdStore.URI)\""
-    uriNotification.font = uriNotification.font?.withSize(CGFloat(14))
+   
     //clear stackView
-    
-    self.filterData = self.credentialIdStore.credentials
+    self.filterCredentials = self.credentialIdStore.credentials
+    self.filterOthers = self.credentialIdStore.otherCredentials
     tableView.reloadData()
     
-//
-//    if credentialIdStore.credentials.count > 0 {
-//      //   label
-//      let label = makeLabel(text: "Credentials for \(credentialIdStore.URI)\n", size: 20)
-//      label.textAlignment = .left
-//      self.stackView.addArrangedSubview(label)
-//      self.stackView.setCustomSpacing(CGFloat(5), after: label)
-//
-//      // add credential items
-//      for (index, item) in self.credentialIdStore.credentials.enumerated() {
-//          let btn = self.makeButton(title: item["username"] ?? "", index: index)
-//          self.stackView.addArrangedSubview(btn)
-//      }
-//      return
-//    }
-//
-//    // No password available
-//    let label = makeLabel(text: "There are no passwords available for this uri. Add a password to Locker to start using autofill feature.", size: 16)
-//    label.frame = CGRect(x: 16, y: 0, width: UIScreen.main.bounds.size.width-60, height: 100.0)
-//    label.textAlignment = .center
-//    self.stackView.addSubview(label)
-    //stackView.alignment = .center
-  }
-
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.filterData.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
     
-    cell.textLabel?.text = self.filterData[indexPath.row]["username"]
-
-    return cell
-  }
-  func numberOfSections(in tableView: UITableView) -> Int {
-    
-    // #warning Incomplete implementation, return the number of sections
-    return 1
-  }
-  
-  //mark search bar config
-  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-    filterData = []
-    if searchText == "" {
-      filterData = credentialIdStore.credentials
-    }
-    for credential in credentialIdStore.credentials {
-      let username: String = credential["username"]!.lowercased()
-      if username.contains(searchText.lowercased()) {
-          filterData.append(credential)
-      }
-    }
-     self.tableView.reloadData()
+    newPassword.uri = self.credentialIdStore.URI
   }
     
-    @IBAction func cancel(_ sender: AnyObject?) {
+  @IBAction func cancel(_ sender: AnyObject?) {
     self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.userCanceled.rawValue))
   }
   
   // move to Add pass screen
   @IBAction func add() {
-
     let addPassView = storyboard?.instantiateViewController(withIdentifier: "AddNewPasswordView") as! NewPasswordViewController
 //    addPassView.completionHandler = {text1, text2 in
 //      print(text1, text2)
@@ -152,3 +64,72 @@ class CredentialProviderViewController: ASCredentialProviderViewController, UISe
 }
 
 
+extension CredentialProviderViewController: UISearchBarDelegate {
+  //mark search bar config, handle search function.
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+  
+    filterCredentials = []
+    filterOthers = []
+    if searchText == "" {
+      filterCredentials = credentialIdStore.credentials
+      filterOthers = credentialIdStore.otherCredentials
+    }
+    // get matches credentiral username
+    for credential in credentialIdStore.credentials {
+      let username: String = credential["username"]!.lowercased()
+      if username.contains(searchText.lowercased()) {
+          filterCredentials.append(credential)
+      }
+    }
+    // for ohters
+    for credential in credentialIdStore.otherCredentials {
+      let username: String = credential["username"]!.lowercased()
+      if username.contains(searchText.lowercased()) {
+          filterOthers.append(credential)
+      }
+    }
+     self.tableView.reloadData()
+  }
+}
+
+
+extension CredentialProviderViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  // section 0 contain credentials for the uri, section 1 for all others
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if section == 0 {
+      return self.filterCredentials.count
+    }
+    
+    return self.filterOthers.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let credential = indexPath.section == 0 ? self.filterCredentials[indexPath.row] : self.filterOthers[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CredentialTableViewCell
+    
+    cell.username.text = credential["username"]
+    cell.uri.text = credential["uri"]
+    cell.setCredentialIconLabel(text: credential["username"]!)
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return section == 0 ? "Passwords for \"\(self.credentialIdStore.URI)\" (\(self.credentialIdStore.credentials.count))" : "All passwords (\(self.credentialIdStore.otherCredentials.count))"
+  }
+  func numberOfSections(in tableView: UITableView) -> Int {
+    
+    // #warning Incomplete implementation, return the number of sections
+    // credential for the uri and other
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    let target = indexPath.section == 0 ? filterCredentials[indexPath.row] : filterOthers[indexPath.row]
+    let passwordCredential = ASPasswordCredential(user: target["username"] ?? "", password: target["password"] ?? "")
+    self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
+
+  }
+}
--- ios/LockerAutofill/NewPasswordViewController.swift
+++ ios/LockerAutofill/NewPasswordViewController.swift
@@ -9,29 +9,29 @@ import UIKit
 
 class NewPasswordViewController: UIViewController {
 
-  @IBOutlet weak var label: UILabel!
-  @IBOutlet weak var userName: UITextField!
-  @IBOutlet weak var password: UITextField!
+
   
+  @IBOutlet weak var uriField: CustomTextField!
+  @IBOutlet weak var passwordField: CustomTextField!
+  @IBOutlet weak var usernameField: CustomTextField!
+  @IBOutlet weak var nameField: CustomTextField!
   //send data back to credential view
   public var completionHandler: ((String?, String?) -> Void)?
   
   override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Add Password"
-        // Do any additional setup after loading the view.
-    }
+    self.uriField.text = newPassword.uri
+  }
     
 
   @IBAction func cancel(_ sender: Any) {
     dismiss(animated: true, completion: nil)
   }
   @IBAction func donePress(_ sender: Any) {
-    let user = userName.text
-    let pass = password.text
-    if user != nil || pass != nil {
-      completionHandler?(user, pass)
-      dismiss(animated: true, completion: nil)
-    }
+
+   
+     
+    dismiss(animated: true, completion: nil)
+    
   }
 }
