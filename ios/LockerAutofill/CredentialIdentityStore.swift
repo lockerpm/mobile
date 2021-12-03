5f200d6d5d041cdbb4ebc99d906950ac03b8ddce
Nguyen Thinh <thinhnn@Nguyens-Mac-mini.local>
2021-12-03 11:58:45 +0700

    add addnew password action

--- ios/LockerAutofill/CredentialIdentityStore.swift
+++ ios/LockerAutofill/CredentialIdentityStore.swift
@@ -13,9 +13,41 @@ struct PasswordCredential {
   var name: String!
   var username: String!
   var password: String!
+//
+//  var uri: String {
+//    get {
+//      return _uri
+//    }
+//    set (newVal) {
+//      _uri = newVal
+//    }
+//  }
+//  var username: String {
+//    get {
+//      return _username
+//    }
+//    set (newVal) {
+//      _username = newVal
+//    }
+//  }
+//  var password: String {
+//    get {
+//      return _password
+//    }
+//    set (newVal) {
+//      _password = newVal
+//    }
+//  }
 }
 
+
+// stupid work :< i will find a better way to handle share data in swift 
+// support for add new password
 var newPassword = PasswordCredential()
+
+// gobal store
+var credentialIdStore: CredentialIdentityStore!
+
 
 class CredentialIdentityStore {
   private let KEYCHAIN_SERVICE: String = "W7S57TNBH5.com.cystack.lockerapp"
@@ -23,6 +55,7 @@ class CredentialIdentityStore {
   
   private let KEYCHAIN_PROPS: String = "autofill"
   private var autofillData: String
+  var passwords: [[String: String]] = []    // convert autofilldata string to array
   private var keychain: Keychain
   
   
@@ -34,11 +67,11 @@ class CredentialIdentityStore {
   init(_ uri: String) {
     self.URI = URL(string: uri)?.host ?? ""
     
-    var passwords: [[String: String]] = []
+    
     // Get data from shared keychain
     self.keychain = Keychain(service: KEYCHAIN_SERVICE, accessGroup: KEYCHAIN_ACCESS_GROUP)
     self.autofillData = try! keychain.get(KEYCHAIN_PROPS) ?? "[]"
-    
+    print(self.autofillData)
     passwords = self.toArray(text: self.autofillData) ?? []
     
     // Buttons
@@ -55,6 +88,15 @@ class CredentialIdentityStore {
     print(self.credentials)
   }
   
+  public func addNewCredential(credential: PasswordCredential) {
+    
+  }
+  public func editCredential(credential: PasswordCredential) {
+    
+  }
+  public func removeCredential(credential: PasswordCredential) {
+    
+  }
   
   
   
