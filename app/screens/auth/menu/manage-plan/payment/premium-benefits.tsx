c0777401947c97ed340c6df17c695a1003c39681
Nguyen Thinh <thinhnn@Mac-mini.localdomain>
2022-03-02 16:37:06 +0700

    UPDATE: payment screen

--- app/screens/auth/menu/manage-plan/payment/premium-benefits.tsx
+++ app/screens/auth/menu/manage-plan/payment/premium-benefits.tsx
@@ -1,36 +1,33 @@
 import React, { useState } from "react"
 import { View, TouchableOpacity } from "react-native"
-import { AutoImage as Image, Text, Layout, Button } from "../../../../../components"
-import { useNavigation } from "@react-navigation/native"
-import { commonStyles, fontSize } from "../../../../../theme"
+import { AutoImage as Image, Text } from "../../../../../components"
+// import { commonStyles, fontSize } from "../../../../../theme"
 import { TabView, SceneMap } from 'react-native-tab-view';
 import { useMixins } from "../../../../../services/mixins"
 
 
 export const PremiumBenefits = function PremiumBenefits() {
   const { translate, color } = useMixins()
-  const navigation = useNavigation()
 
   const tabs = [
     {
-      img: require("./intro.png"),
-      desc: "Unlimited Passwords, Secure Notes, Payment Cards, Identities, Crypto Assets"
+      img: require("./Locker.png"),
+      desc: translate("payment.benefit.locker")
     },
     {
-      img: require("./intro.png"),
-      desc: "Dark Web Monitoring protects your accounts from massive data breaches"
+      img: require("./Web.png"),
+      desc: translate("payment.benefit.web")
     },
     {
-      img: require("./intro.png"),
-      desc: "Emergency Contact let trusted accounts access your vault when needed"
+      img: require("./EmergencyContact.png"),
+      desc: translate("payment.benefit.emergency_contact")
     },
     {
-      img: require("./intro.png"),
-      desc: "Share Passwords and items securely with your friends and family members."
+      img: require("./SharePassword.png"),
+      desc: translate("payment.benefit.share_password")
     }
   ]
   const map = {}
-
   const [index, setIndex] = useState(0)
   const [routes] = useState(tabs.map((item, index) => ({
     key: index.toString()
@@ -46,44 +43,43 @@ export const PremiumBenefits = function PremiumBenefits() {
           alignItems: 'center'
         }}
       >
-        <Image source={item.img} style={{height:120, marginBottom: 30}} />
-        <Text text={item.desc} style={{ textAlign: 'center', lineHeight: 24, maxWidth: 320 }} />
+        <Image source={item.img} style={{ marginBottom: 30, height: "65%" }} />
+        <Text preset="black" text={item.desc} style={{ textAlign: 'center', lineHeight: 24, maxWidth: 320, marginBottom: 5 }} />
       </View>
     )
   })
   const renderScene = SceneMap(map)
 
   return (
-      <View style={{ flex: 1, justifyContent: 'center', position: "absolute", width: "100%" }}>
-        {/* Tabs */}
-        <View style={{ flex: 3, minHeight: 300, maxHeight: 500 }}>
-          <TabView
-            renderTabBar={() => null}
-            navigationState={{ index, routes }}
-            renderScene={renderScene}
-            onIndexChange={setIndex}
-          />
-        </View>
-
-        {/* Bullets */}
-        <View style={{ flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
-          {
-            routes.map((item, i) =>
-              <TouchableOpacity
-                key={i}
-                style={{
-                  height: 4,
-                  width: 12,
-                  borderRadius: 25,
-                  marginVertical: 15,
-                  marginHorizontal: 4,
-                  backgroundColor: i === index ? color.primary : color.palette.lightGray
-                }}
-                onPress={() => setIndex(i)}
-              />
-            )
-          }
-        </View>
+    <View style={{ width: "100%" }}>
+      {/* Tabs */}
+      <View style={{ flexBasis: "70%", marginBottom: 5 }}>
+        <TabView
+          renderTabBar={() => null}
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+        />
       </View>
+
+      {/* Bullets */}
+      <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 16 }}>
+        {
+          routes.map((item, i) =>
+            <TouchableOpacity
+              key={i}
+              style={{
+                height: 4,
+                width: 12,
+                borderRadius: 25,
+                marginHorizontal: 4,
+                backgroundColor: i === index ? color.primary : color.palette.lightGray
+              }}
+              onPress={() => setIndex(i)}
+            />
+          )
+        }
+      </View>
+    </View>
   )
 }
