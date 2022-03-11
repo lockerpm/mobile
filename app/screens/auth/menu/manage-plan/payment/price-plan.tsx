4b93423730466d9256979b0bb31cf16f636d5a3c
Nguyen Thinh <thinhnn@Mac-mini.localdomain>
2022-03-11 12:26:36 +0700

    add dark view for payment screen

--- app/screens/auth/menu/manage-plan/payment/price-plan.tsx
+++ app/screens/auth/menu/manage-plan/payment/price-plan.tsx
@@ -3,7 +3,7 @@ import { View, TouchableOpacity } from "react-native"
 import CheckBox from "@react-native-community/checkbox"
 import { Button, Text } from "../../../../../components"
 import { useMixins } from "../../../../../services/mixins"
-
+import { observer } from "mobx-react-lite"
 
 interface PricePlanItemProps {
   onPress: () => void
@@ -13,12 +13,13 @@ interface PricePlanItemProps {
   subtitle: string
 }
 
-export const PricePlanItem = (prop: PricePlanItemProps) => {
+const PricePlanItem = (prop: PricePlanItemProps) => {
+  const { color } = useMixins()
   return (
     <TouchableOpacity
       onPress={prop.onPress}
       style={{
-        backgroundColor: prop.isEnable ? "#E8E8E9" : "white",
+        backgroundColor: prop.isEnable ? color.block : color.background,
         height: 67,
         borderRadius: 10,
         flexDirection: "row",
@@ -49,22 +50,24 @@ export const PricePlanItem = (prop: PricePlanItemProps) => {
 
 interface PricePlanProps {
   onPress: React.Dispatch<React.SetStateAction<boolean>>
-  purchase: () => void
+  purchase: (subID: string) => void
   isEnable: boolean
   personal: boolean
 }
 
-export const PricePlan = (prop: PricePlanProps) => {
-  const { translate } = useMixins()
-  const priceText = {
+export const PricePlan = (prop: PricePlanProps) => { 
+  const { translate, color } = useMixins()
+  const planText = {
     per : {
       monthly: {
+        subId: "pm_premium_monthly",
         title: translate("payment.price.per.monthly.title"),
         subtitle: translate("payment.price.per.monthly.subtitle"),
         onSale:  translate("payment.price.per.monthly.sale"),
         pay_title: translate("payment.price.per.monthly.pay_title")
       },
       yearly: {
+        subId:  "pm_premium_yearly",
         title: translate("payment.price.per.yearly.title"),
         subtitle:  translate("payment.price.per.yearly.subtitle"),
         onSale: translate("payment.price.per.yearly.sale"),
@@ -73,12 +76,14 @@ export const PricePlan = (prop: PricePlanProps) => {
     },
     fam : {
       monthly: {
+        subId: "pm_family_monthly",
         title:  translate("payment.price.fam.monthly.title"),
         subtitle:  translate("payment.price.fam.monthly.subtitle"),
         onSale:  translate("payment.price.fam.monthly.sale"),
         pay_title:  translate("payment.price.fam.monthly.pay_title") 
       },
       yearly: {
+        subId: "pm_family_yearly",
         title:  translate("payment.price.fam.yearly.title"),
         subtitle:  translate("payment.price.fam.yearly.subtitle"),
         onSale:  translate("payment.price.fam.yearly.sale"),
@@ -87,7 +92,8 @@ export const PricePlan = (prop: PricePlanProps) => {
     }
   }
 
-  const price = prop.personal? priceText.per : priceText.fam
+  const plan = prop.personal? planText.per : planText.fam
+  const billingCycle = prop.isEnable ? plan.yearly : plan.monthly
 
   return (
     <View style={{ marginLeft: 20, marginRight: 20 }}>
@@ -98,25 +104,25 @@ export const PricePlan = (prop: PricePlanProps) => {
       <PricePlanItem
         onPress={() => prop.onPress(true)}
         isEnable={prop.isEnable}
-        onSale={price.yearly.onSale}
-        title={price.yearly.title}
-        subtitle={price.yearly.subtitle}
+        onSale={plan.yearly.onSale}
+        title={plan.yearly.title}
+        subtitle={plan.yearly.subtitle}
       />
       <PricePlanItem
         onPress={() => prop.onPress(false)}
         isEnable={!prop.isEnable}
-        onSale={price.monthly.onSale}
-        title={price.monthly.title}
-        subtitle={price.monthly.subtitle}
+        onSale={plan.monthly.onSale}
+        title={plan.monthly.title}
+        subtitle={plan.monthly.subtitle}
       />
       <Button style={{
         flexDirection: "column",
         marginTop: 20,
-      }} onPress={prop.purchase}>
-        <Text style={{ fontSize: 16, fontWeight: "600", fontStyle: "normal", color: "white" }}>
-          {prop.isEnable ? price.yearly.pay_title : price.monthly.pay_title}
+      }} onPress={() => prop.purchase(billingCycle.subId)}>
+        <Text preset="bold" style={{ color: color.white}}>
+          {billingCycle.pay_title}
         </Text>
-        <Text style={{ fontSize: 12, color: "white" }}>{translate("payment.cancel_text")}</Text>
+        <Text style={{ fontSize: 12, color: color.white}}>{translate("payment.cancel_text")}</Text>
       </Button>
     </View>
   )
