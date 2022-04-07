ecb458e1d7fcd15f3e7ce18ddd2efc9abd46a0c4
duchm <duchm@duchms-Mac-mini.local>
2022-04-07 11:57:46 +0700

    feat: notify user when using temp id to send api

--- app/screens/auth/start/start-screen.tsx
+++ app/screens/auth/start/start-screen.tsx
@@ -38,6 +38,12 @@ export const StartScreen = observer(() => {
       syncAutofillData()
     }
 
+    // Testing
+    // if (__DEV__) {
+    //   navigation.navigate('dataOutdated')
+    //   return
+    // }
+
     const connectionState = await NetInfo.fetch()
 
     // Sync
