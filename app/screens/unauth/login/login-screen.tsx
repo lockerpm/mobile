615d6dfac22c91fd3ee2ea4ff57425d66885a7c9
Nguyen Ngoc Thinh <thinh.nn3386@gmail.com>
2022-04-12 14:40:27 +0700

    fix: can not read env build release

--- app/screens/unauth/login/login-screen.tsx
+++ app/screens/unauth/login/login-screen.tsx
@@ -9,7 +9,6 @@ import { DefaultLogin } from "./default"
 import { MethodSelection } from "./method-selection"
 import { Otp } from "./otp"
 import { useStores } from "../../../models"
-import { BASE_URL } from "../../../config/constants"
 
 export const LoginScreen = observer(() => {
   const navigation = useNavigation()
