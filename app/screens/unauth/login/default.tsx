88c75ba160364f6bfcd499edaf2050ddb7707de2
duchm <duchm@duchms-Mac-mini.local>
2022-04-18 10:31:30 +0700

    style: update social login icons

--- app/screens/unauth/login/default.tsx
+++ app/screens/unauth/login/default.tsx
@@ -79,16 +79,12 @@ export const DefaultLogin = observer((props: Props) => {
   const SOCIAL_LOGIN: {
     [service: string]: {
       hide?: boolean
-      size?: number
-      marginBottom?: number
       icon: any
       handler: () => void
     }
   } = {
     apple: {
       hide: !IS_IOS,
-      size: 34,
-      marginBottom: 4,
       icon: uiStore.isDark ? SOCIAL_LOGIN_ICON.appleLight : SOCIAL_LOGIN_ICON.apple,
       handler: () => {
         return appleLogin({
@@ -209,14 +205,7 @@ export const DefaultLogin = observer((props: Props) => {
                 onPress={item.handler}
                 style={{ marginHorizontal: 10 }}
               >
-                <Image
-                  source={item.icon}
-                  style={{ 
-                    height: item.size || 30, 
-                    width: item.size || 30,
-                    marginBottom: item.marginBottom || 0
-                  }}
-                />
+                <item.icon height={40} width={40} />
               </Button>
             ))
           }
