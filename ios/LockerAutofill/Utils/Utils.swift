d80e34faa83c933868803b7b5e86be7e73dde1f2
Nguyen Thinh <thinhnn@Nguyens-Mini.localdomain>
2022-04-19 17:04:23 +0700

    chore: revert commit ea7326b

--- ios/LockerAutofill/Utils/Utils.swift
+++ ios/LockerAutofill/Utils/Utils.swift
@@ -13,13 +13,6 @@ class Utils {
 
   static private let pbkdf2: HashCore = HashCore()
   
-  static public func GetKeychainKeyService() -> String {
-    if let appIdPrefix = ProcessInfo.processInfo.environment["APP_ID_PREFIX"] {
-      print(appIdPrefix)
-      return appIdPrefix + ".com.cystack.lockerapp"
-    }
-    return ""
-  }
   static public func Translate(_ key: String) -> String {
     return key.locolized()
   }
