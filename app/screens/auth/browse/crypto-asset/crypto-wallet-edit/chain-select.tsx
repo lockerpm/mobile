fd2eecf9e9bb28bc7690bb1e9efb66bf51b6031c
duchm <duchm@duchms-Mac-mini.local>
2022-05-09 15:24:06 +0700

    style: add custom renderItem to Select

--- app/screens/auth/browse/crypto-asset/crypto-wallet-edit/chain-select.tsx
+++ app/screens/auth/browse/crypto-asset/crypto-wallet-edit/chain-select.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
+import { ImageStyle, View, ViewStyle } from 'react-native'
 import { Text, Select, AutoImage as Image } from '../../../../../components'
 import { useMixins } from '../../../../../services/mixins'
 import { commonStyles, fontSize } from '../../../../../theme'
 import { CHAIN_LIST } from '../../../../../utils/crypto/chainlist'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
+import IonIconsIcon from 'react-native-vector-icons/Ionicons'
 
 
 type Props = {
@@ -19,7 +20,32 @@ export const ChainSelect = (props: Props) => {
   const { onChange, selected } = props
   const { translate, color } = useMixins()
 
-  const otherChain = CHAIN_LIST.find(c => c.alias === 'other')
+  // ------------------ METHODS ------------------
+
+  const findChain = (al: string) => {
+    return CHAIN_LIST.find(c => c.alias === al)
+  }
+
+  // ------------------ COMPUTED ------------------
+
+  const otherChain = findChain('other')
+
+  // ------------------ RENDER ------------------
+
+  const IMG_CONTAINER: ViewStyle = {
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: color.line
+  }
+
+  const IMG: ImageStyle = {
+    borderRadius: 20,
+    height: 40,
+    width: 40,
+    backgroundColor: 'white',
+  }
 
   return (
     <Select
@@ -27,7 +53,7 @@ export const ChainSelect = (props: Props) => {
       multiple
       value={selected.map(i => i.alias)}
       onChange={(values) => {
-        const chains = values.map((alias: string) => CHAIN_LIST.find(c => c.alias === alias))
+        const chains = values.map((alias: string) => findChain(alias))
         onChange(chains)
       }}
       options={CHAIN_LIST.map(c => ({
@@ -35,6 +61,35 @@ export const ChainSelect = (props: Props) => {
         value: c.alias
       }))}
       title={translate('crypto_asset.network')}
+      renderItem={(value, { isSelected }, itemLabel) => (
+        <View style={[commonStyles.CENTER_HORIZONTAL_VIEW, {
+          backgroundColor: color.background,
+          paddingHorizontal: 16,
+          paddingVertical: 8
+        }]}>
+          <View style={IMG_CONTAINER}>
+            <Image
+              source={findChain(value)?.logo || otherChain.logo}
+              borderRadius={20}
+              style={IMG}
+            />
+          </View>
+          <Text
+            preset='black'
+            text={itemLabel}
+            style={{ flex: 1, marginRight: 20 }}
+          />
+          {
+            isSelected && (
+              <IonIconsIcon
+                name='checkmark'
+                color={color.primary}
+                size={24}
+              />
+            )
+          }
+        </View>
+      )}
       renderSelected={() => (
         <View style={{ flex: 1 }}>
           <View
@@ -54,7 +109,7 @@ export const ChainSelect = (props: Props) => {
                 {
                   selected.length ? (
                     selected.map((item) => {
-                      const selectedChain = CHAIN_LIST.find(c => c.alias === item.alias)
+                      const selectedChain = findChain(item.alias)
                       return (
                         <View
                           key={item.alias}
@@ -63,24 +118,11 @@ export const ChainSelect = (props: Props) => {
                             marginVertical: 2
                           }]}
                         >
-                          <View
-                            style={{
-                              borderRadius: 20,
-                              overflow: 'hidden',
-                              marginRight: 10,
-                              borderWidth: 1,
-                              borderColor: color.line
-                            }}
-                          >
+                          <View style={IMG_CONTAINER}>
                             <Image
                               source={selectedChain?.logo || otherChain.logo}
                               borderRadius={20}
-                              style={{
-                                borderRadius: 20,
-                                height: 40,
-                                width: 40,
-                                backgroundColor: 'white'
-                              }}
+                              style={IMG}
                             />
                           </View>
 
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   env: {
-    production: {},
+    production: {
+      "plugins": [
+        [
+            "transform-remove-console",
+            {
+                "exclude": [
+                    "error",
+                    "warn"
+                ]
+            }
+        ]
+      ]
+    },
   },
   plugins: [
     [
