95b07498aac79656432f9001fdd9e6fca891a2c6
duchm <duchm@MacBook-Pro.local>
2022-08-18 15:57:16 +0700

    feat: check team policy when create/edit password

---
+++ app/config/types/api.ts
@@ -0,0 +1,152 @@
+import {
+  AccountRoleText,
+  EmergencyAccessStatus,
+  EmergencyAccessType,
+  PolicyType,
+  SharingStatus,
+  SharingType,
+} from '.'
+
+export type SessionSnapshot = {
+  access_token: string
+  refresh_token: string
+  key: string
+  private_key: string
+}
+
+export type PasswordPolicy = {
+  policy_type: PolicyType.PASSWORD_REQ
+  enabled: boolean
+  config: {
+    min_length: number
+    require_digit: boolean
+    require_lower_case: boolean
+    require_special_character: boolean
+    require_upper_case: boolean
+  }
+}
+
+export type MasterPasswordPolicy = {
+  policy_type: PolicyType.MASTER_PASSWORD_REQ
+  enabled: boolean
+  config: {
+    min_length: number
+    require_digit: boolean
+    require_lower_case: boolean
+    require_special_character: boolean
+    require_upper_case: boolean
+  }
+}
+
+export type BlockFailedLoginPolicy = {
+  policy_type: PolicyType.BLOCK_FAILED_LOGIN
+  enabled: boolean
+  config: {
+    failed_login_attempts: number
+    failed_login_block_time: number
+    failed_login_duration: number
+    failed_login_owner_email: boolean
+  }
+}
+
+export type PasswordlessPolicy = {
+  policy_type: PolicyType.PASSWORDLESS
+  enabled: boolean
+  config: {
+    only_allow_passwordless: boolean
+  }
+}
+
+export type TeamPolicies = [
+  PasswordPolicy,
+  MasterPasswordPolicy,
+  BlockFailedLoginPolicy,
+  PasswordlessPolicy
+]
+
+export type SharingInvitationType = {
+  access_time: number
+  cipher_type: number
+  id: string
+  item_type: string
+  owner: {
+    email: string
+    full_name: string
+  }
+  role: AccountRoleText
+  share_type: SharingType
+  status: SharingStatus
+  hide_passwords: boolean
+  team: {
+    id: string
+    name: string
+    organization_id: string
+  }
+}
+
+export type SharedMemberType = {
+  access_time: string
+  avatar: string
+  email: string
+  full_name: string
+  hide_passwords: boolean
+  id: string
+  pwd_user_id: string
+  role: AccountRoleText
+  share_type: SharingType
+  status: SharingStatus
+  username: string
+}
+
+export type MyShareType = {
+  id: string
+  description: string
+  name: string
+  organization_id: string
+  members: SharedMemberType[]
+}
+
+export type UserTeam = {
+  id: string
+  description: string
+  name: string
+  role: AccountRoleText
+  creation_date: number
+  revision_date: number
+  is_business: boolean
+  locker: boolean
+  organization_id: string
+}
+
+export type RelayAddress = {
+  address: string
+  created_time: number
+  description: string
+  domain: string
+  enabled: boolean
+  full_address: string
+  id: number
+  num_blocked: number
+  num_forwarded: number
+  num_replied: number
+  num_spam: number
+  updated_time: null
+}
+
+export type TrustedContact = {
+  avatar: string
+  creation_date: number
+  email: string
+  full_name: string
+  grantor_pwd_user_id?: string
+  grantee_pwd_user_id?: string
+  id: string
+  key_encrypted: string
+  last_notification_date: number | null
+  object: string
+  recovery_initiated_date: number | null
+  revision_date: number
+  status: EmergencyAccessStatus
+  type: EmergencyAccessType
+  wait_time_days: number
+}
--- app/screens/auth/tools/private-relay/private-relay.tsx
+++ app/screens/auth/tools/private-relay/private-relay.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from "react"
-import { observer } from "mobx-react-lite"
-import { Animated, TouchableOpacity, View } from "react-native"
-import { Layout, Header, Text, AutoImage as Imgae, Button } from "../../../../components"
-import { useNavigation } from "@react-navigation/native"
-import { commonStyles } from "../../../../theme"
+import React, { useEffect, useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import { Animated, TouchableOpacity, View } from 'react-native'
+import { Layout, Header, Text, AutoImage as Imgae, Button } from '../../../../components'
+import { useNavigation } from '@react-navigation/native'
+import { commonStyles } from '../../../../theme'
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
 import Entypo from 'react-native-vector-icons/Entypo'
-import { useMixins } from "../../../../services/mixins"
-import { AliasItem } from "./private-relay-item"
-import { useStores } from "../../../../models"
-import { RelayAddress } from "../../../../services/api"
-import { EditAliasModal } from "./edit-alias-modal"
-
+import { useMixins } from '../../../../services/mixins'
+import { AliasItem } from './private-relay-item'
+import { useStores } from '../../../../models'
+import { EditAliasModal } from './edit-alias-modal'
+import { RelayAddress } from '../../../../config/types/api'
 
 export const PrivateRelay = observer(() => {
   const navigation = useNavigation()
@@ -30,7 +29,7 @@ export const PrivateRelay = observer(() => {
 
   const fetchRelayListAddressed = async () => {
     const res = await toolStore.fetchRelayListAddresses()
-    if (res.kind === "ok") {
+    if (res.kind === 'ok') {
       setShowDesc(res.data.count === 0)
       setAlias(res.data.results)
     }
@@ -38,7 +37,7 @@ export const PrivateRelay = observer(() => {
 
   const generateRelayNewAddress = async () => {
     const res = await toolStore.generateRelayNewAddress()
-    if (res.kind === "ok") {
+    if (res.kind === 'ok') {
       const newList = [...alias]
       newList.push(res.data)
       setAlias(newList)
@@ -46,7 +45,7 @@ export const PrivateRelay = observer(() => {
   }
 
   const deleteRelayAddress = (id: number) => {
-    const newList = alias.filter(a => a.id !== id)
+    const newList = alias.filter((a) => a.id !== id)
     setAlias(newList)
   }
 
@@ -63,37 +62,38 @@ export const PrivateRelay = observer(() => {
     <Layout
       containerStyle={{
         backgroundColor: color.block,
-        paddingHorizontal: 0
+        paddingHorizontal: 0,
       }}
-      header={(
+      header={
         <Header
           title={translate('private_relay.title')}
           goBack={() => navigation.goBack()}
-          right={(
+          right={
             <Button
               disabled={alias.length >= 5}
               textStyle={{
-                color: alias.length >= 5 ? color.block : color.primary
+                color: alias.length >= 5 ? color.block : color.primary,
               }}
               preset="link"
               text={translate('private_relay.btn')}
               onPress={() => {
                 generateRelayNewAddress()
               }}
-
             />
-          )}
-        />
-      )}
-    >
-      {
-        alias.length > 0 && <EditAliasModal
-          item={alias[0]}
-          isOpen={showEditModal}
-          onClose={() => { setShowEditModal(false) }}
-          onEdit={onEdit}
+          }
         />
       }
+    >
+      {alias.length > 0 && (
+        <EditAliasModal
+          item={alias[0]}
+          isOpen={showEditModal}
+          onClose={() => {
+            setShowEditModal(false)
+          }}
+          onEdit={onEdit}
+        />
+      )}
       {/* Root email start */}
       <View style={[commonStyles.SECTION_PADDING, { backgroundColor: color.background }]}>
         <TouchableOpacity
@@ -112,36 +112,42 @@ export const PrivateRelay = observer(() => {
             paddingLeft: 16,
           }}
         >
-          <View style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center"
-          }}>
-            <View style={{ flexDirection: "row" }}>
-              <Imgae source={require('./root-email.png')} style={{
-                width: 36,
-                height: 36
-              }} />
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flexDirection: 'row' }}>
+              <Imgae
+                source={require('./root-email.png')}
+                style={{
+                  width: 36,
+                  height: 36,
+                }}
+              />
               <View style={{ marginLeft: 8 }}>
                 <Text text={translate('private_relay.root_email')} />
                 <Text preset="semibold" text={user.email} />
               </View>
             </View>
-            <EvilIcons name={showDesc ? "chevron-up" : "chevron-down"} size={24} />
+            <EvilIcons name={showDesc ? 'chevron-up' : 'chevron-down'} size={24} />
           </View>
 
-          {
-            showDesc && <Animated.View style={{ marginTop: 8 }}>
-              {
-                rootEmailDesc.map((item, index) => (
-                  <View key={index} style={{ flexDirection: "row", marginRight: 16, marginVertical: 2 }}>
-                    <Entypo name="dot-single" size={16} />
-                    <Text text={item} />
-                  </View>
-                ))
-              }
+          {showDesc && (
+            <Animated.View style={{ marginTop: 8 }}>
+              {rootEmailDesc.map((item, index) => (
+                <View
+                  key={index}
+                  style={{ flexDirection: 'row', marginRight: 16, marginVertical: 2 }}
+                >
+                  <Entypo name="dot-single" size={16} />
+                  <Text text={item} />
+                </View>
+              ))}
             </Animated.View>
-          }
+          )}
         </TouchableOpacity>
       </View>
       {/* Root email end */}
@@ -149,30 +155,30 @@ export const PrivateRelay = observer(() => {
         text={translate('private_relay.label') + ` (${alias.length}/5)`}
         style={{
           marginLeft: 20,
-          marginVertical: 4
+          marginVertical: 4,
         }}
       />
       {/* emails */}
       <View
-        style={[commonStyles.SECTION_PADDING, {
-          marginTop: 10,
-          backgroundColor: color.background
-        }]}
+        style={[
+          commonStyles.SECTION_PADDING,
+          {
+            marginTop: 10,
+            backgroundColor: color.background,
+          },
+        ]}
       >
-
-        {
-          alias.map((item, index) => (
-            <AliasItem
-              key={index}
-              index={index}
-              item={item}
-              deleteRelayAddress={deleteRelayAddress}
-              setShowEditModal={() => setShowEditModal(true)}
-            />
-          ))
-        }
+        {alias.map((item, index) => (
+          <AliasItem
+            key={index}
+            index={index}
+            item={item}
+            deleteRelayAddress={deleteRelayAddress}
+            setShowEditModal={() => setShowEditModal(true)}
+          />
+        ))}
       </View>
       {/* emails end */}
-    </Layout >
+    </Layout>
   )
 })
