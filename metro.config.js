d0a9038772be0bf656c3c85d744213d9b953b2f0
Nguyen Thinh <thinhnn@Nguyens-Mac-mini-3.local>
2023-09-27 16:54:54 +0700

    refactor: remove vector icon

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,13 @@
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('metro-config')
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  } = await getDefaultConfig()
   return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
-  };
-})();
+  }
+})()
