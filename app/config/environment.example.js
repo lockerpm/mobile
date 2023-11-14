1cae76362a787115c80930c7b2f50b07b49c737a
Nguyen Thinh <thinhnn@Nguyens-Mac-mini-3.local>
2023-11-14 15:16:30 +0700

    chore: edit text

--- app/config/environment.example.js
+++ app/config/environment.example.js
@@ -1,33 +1,35 @@
-
 /**
  * The base api url
  * Please setup your server follow @see https://github.com/lockerpm/api
  */
-BASE_URL = ''
-WS_URL = ''
+BASE_URL = ""
+/**
+ * Socket
+ */
+WS_URL = ""
 
 /**
  * Your web address you config for locker web
  * This will display the item on the web when you quickly share your vault item
+ * (Required when quick share item)
  */
-QUICK_SHARE_BASE_URL = ''
+QUICK_SHARE_BASE_URL = ""
 
 /**
- * Get website logo to display in item logo
+ * Get website logo to display in item logo (OPTIONAL)
  */
-GET_LOGO_URL = ''
-
-
-/**
- * Some web address
- */
-TERMS_URL = ''
-PRIVACY_POLICY_URL = ''
-HELP_CENTER_URL = ''
-REPORT_VULN = ''
+GET_LOGO_URL = ""
 
 /**
- * Add captcha code when registering or forgetting password
+ * Some web address (OPTIONAL)
  */
-RECAPTCHA_SITE_KEY = ''
-RECAPTCHA_BASE_URL = ''
+TERMS_URL = ""
+PRIVACY_POLICY_URL = ""
+HELP_CENTER_URL = ""
+REPORT_VULN = ""
+
+/**
+ * Add captcha code when registering or forgetting password (OPTIONAL)
+ */
+RECAPTCHA_SITE_KEY = ""
+RECAPTCHA_BASE_URL = ""
