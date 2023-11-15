6843d43d082e5fbb1881df07c46ddcae2133bcf1
Nguyen Thinh <thinhnn@Nguyens-Mac-mini-3.local>
2023-11-15 12:34:32 +0700

    chore: remove Universal Links

--- ios/Locker/AppDelegate.mm
+++ ios/Locker/AppDelegate.mm
@@ -74,11 +74,6 @@
   return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options];
 }
 
-// Universal Links
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
-  BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result;
-}
 
 
 
