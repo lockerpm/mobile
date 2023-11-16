ce4f4e45a0306edc7b5cb67be84e8e17b44a39db
Nguyen Thinh <thinhnn@Nguyens-Mac-mini-3.local>
2023-11-16 17:23:07 +0700

    fix: emergency view shared item

--- app/screens/auth/menu/settings/emergencyAccess/view/ViewScreen.tsx
+++ app/screens/auth/menu/settings/emergencyAccess/view/ViewScreen.tsx
@@ -8,16 +8,18 @@ import { useStores } from "app/models"
 import { useCipherData, useCipherHelper, useHelper } from "app/services/hook"
 import { TrustedContact } from "app/static/types"
 import { CipherData } from "core/models/data"
-import { Cipher } from "core/models/domain"
+import { Cipher, EncString, SymmetricCryptoKey } from "core/models/domain"
 import { CipherView } from "core/models/view"
 import { Header, Screen, Text } from "app/components/cores"
 import { AppStackScreenProps } from "app/navigators/navigators.types"
+import { useCoreService } from "app/services/coreService"
 
 export const ViewEAScreen: FC<AppStackScreenProps<"viewEA">> = observer((props) => {
   const navigation = props.navigation
   const route = props.route
 
-  const { translate } = useHelper()
+  const { cryptoService, cipherService } = useCoreService()
+  const { translate, notify } = useHelper()
   const { user } = useStores()
 
   const { getEncKeyFromDecryptedKey } = useCipherData()
@@ -35,26 +37,70 @@ export const ViewEAScreen: FC<AppStackScreenProps<"viewEA">> = observer((props) 
   const [ciphers, setCiphers] = useState([])
 
   // -------------- METHOD ------------------
+
+  const getAllCiphers = async (response) => {
+    // Get enc key
+    let encKey
+    try {
+      // const keyBuffer = await cryptoService.rsaDecrypt(response.key_encrypted)
+      encKey = await getEncKeyFromDecryptedKey(response.key_encrypted)
+    } catch (error) {
+      notify("error", "Invalid key, cannot decrypt data")
+      return []
+    }
+
+    // Decrypt orgs
+    const orgMap = {}
+    try {
+      const privateKey = await cryptoService.decryptToBytes(
+        new EncString(response.private_key),
+        encKey,
+      )
+      const promises = []
+      const _getOrgKey = async (id, key) => {
+        const decKeyBuffer = await cryptoService.rsaDecrypt(key, privateKey)
+        orgMap[id] = new SymmetricCryptoKey(decKeyBuffer)
+      }
+      response.organizations.forEach((org) => {
+        promises.push(_getOrgKey(org.id, org.key))
+      })
+      await Promise.allSettled(promises)
+    } catch (error) {
+      //
+    }
+
+    // Decrypt ciphers
+    const decCiphers = []
+
+    try {
+      const ciphers = response.ciphers
+      const promises = []
+      const _decryptCipher = async (cipherResponse) => {
+        const cipherData = new CipherData(cipherResponse)
+        const cipher = new Cipher(cipherData)
+        const decCipher = await cipher.decrypt(orgMap[cipherResponse.organization_id] || encKey)
+        decCiphers.push(decCipher)
+      }
+      ciphers.forEach((cipherResponse) => {
+        promises.push(_decryptCipher(cipherResponse))
+      })
+      await Promise.allSettled(promises)
+    } catch (error) {
+      //
+    }
+    // Done, process new types and sort
+    decCiphers.sort(cipherService.getLocaleSortingFunction())
+    return decCiphers
+  }
+
   const mount = async () => {
     setIsLoading(true)
     const fetchKeyRes = await user.viewEA(trustContact.id)
     if (fetchKeyRes.kind !== "ok") return
+
     try {
-      const encKey = await getEncKeyFromDecryptedKey(fetchKeyRes.data.key_encrypted)
-      const decCiphers = []
-      const promises = []
+      const decCiphers = await getAllCiphers(fetchKeyRes.data)
 
-      fetchKeyRes.data.ciphers.forEach((cipherResponse) => {
-        const cipherData = new CipherData(cipherResponse)
-        const cipher = new Cipher(cipherData)
-        promises.push(
-          cipher.decrypt(encKey).then((c) => {
-            decCiphers.push(c)
-          }),
-        )
-      })
-
-      await Promise.all(promises)
       let res = decCiphers.map((c: CipherView) => {
         const cipherInfo = getCipherInfo(c)
         const data = {
