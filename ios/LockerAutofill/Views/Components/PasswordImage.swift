7dd9070a5c02bdc74b93bbeb1ccf65a26f4e8f03
Nguyen Thinh <thinhnn@Nguyens-Mac-mini-3.local>
2024-03-19 17:13:55 +0700

    build: v1.5.16;

--- ios/LockerAutofill/Views/Components/PasswordImage.swift
+++ ios/LockerAutofill/Views/Components/PasswordImage.swift
@@ -11,42 +11,47 @@ import CachedAsyncImage
 struct PasswordImage: View {
   var itemUri: String
   
-  var websiteLogo: String {
-    get {
-      let url = URL(string: itemUri)
-      let domain = url?.host ?? ""
-      return domain.isEmpty ? "" : "https://locker.io/logo/\(domain)?size=120"
-    }
-  }
+  //  var websiteLogo: String {
+  //    get {
+  //      let url = URL(string: itemUri)
+  //      let domain = url?.host ?? ""
+  //      return domain.isEmpty ? "" : "https://locker.io/logo/\(domain)?size=120"
+  //    }
+  //  }
   var body: some View {
-    if websiteLogo.isEmpty {
-      Image("password")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 40, height: 40)
-        .clipShape(RoundedRectangle(cornerRadius: 12))
-    } else {
-      CachedAsyncImage(url: URL(string: websiteLogo)) {  phase in
-        switch phase {
-        case .empty:
-          RoundedRectangle(cornerRadius: 12)
-            .frame(width: 40, height: 40)
-            .background(.gray)
-        case .success(let image):
-          image
-            .resizable()
-        case .failure(_):
-          Image("password")
-            .resizable()
-        @unknown default:
-          Image("password")
-            .resizable()
-        }
-      }
+//    if websiteLogo.isEmpty {
+//      Image("password")
+//        .resizable()
+//        .scaledToFit()
+//        .frame(width: 40, height: 40)
+//        .clipShape(RoundedRectangle(cornerRadius: 12))
+//    } else {
+//      CachedAsyncImage(url: URL(string: websiteLogo)) {  phase in
+//        switch phase {
+//        case .empty:
+//          RoundedRectangle(cornerRadius: 12)
+//            .frame(width: 40, height: 40)
+//            .background(.gray)
+//        case .success(let image):
+//          image
+//            .resizable()
+//        case .failure(_):
+//          Image("password")
+//            .resizable()
+//        @unknown default:
+//          Image("password")
+//            .resizable()
+//        }
+//      }
+//      .scaledToFit()
+//      .frame(width: 40, height: 40)
+//      .clipShape(RoundedRectangle(cornerRadius: 12))
+//    }
+    Image("password")
+      .resizable()
       .scaledToFit()
       .frame(width: 40, height: 40)
       .clipShape(RoundedRectangle(cornerRadius: 12))
-    }
   }
 }
 
